24677b025929c19ffebafe06474d065e0c360db8
nr <nr@narola.email>
2022-05-28 10:11:38 +0530

    First Commit
    Project created

---
+++ Topcoders/Topcoders/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Topcoders
+//
+//  Created by C100-07 on 28/05/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
